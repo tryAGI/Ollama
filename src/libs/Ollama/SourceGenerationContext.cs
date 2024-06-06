c8bcb7dbe8cff21adf45097e886dd4e9ecca9808
HavenDV <havendv@gmail.com>
2024-06-06 11:19:11 +0400

    feat: Updated OpenApiGenerator.

--- src/libs/Ollama/SourceGenerationContext.cs
+++ src/libs/Ollama/SourceGenerationContext.cs
@@ -8,14 +8,20 @@ namespace Ollama;
     Converters =
     [
         // We need specify explicitly all converters for enums inside AnyOf/OneOf/AllOf to make them work
+        typeof(GenerateCompletionRequestFormatJsonConverter),
+        typeof(GenerateCompletionRequestFormatNullableJsonConverter),
+        typeof(GenerateChatCompletionRequestFormatJsonConverter),
+        typeof(GenerateChatCompletionRequestFormatNullableJsonConverter),
+        typeof(GenerateChatCompletionResponseDoneReasonJsonConverter),
+        typeof(GenerateChatCompletionResponseDoneReasonNullableJsonConverter),
+        typeof(MessageRoleJsonConverter),
+        typeof(MessageRoleNullableJsonConverter),
         typeof(CreateModelResponseStatusJsonConverter),
         typeof(CreateModelResponseStatusNullableJsonConverter),
         typeof(PullModelResponseStatusJsonConverter),
         typeof(PullModelResponseStatusNullableJsonConverter),
         typeof(PushModelResponseStatusJsonConverter),
         typeof(PushModelResponseStatusNullableJsonConverter),
-        typeof(GenerateChatCompletionResponseDoneReasonJsonConverter),
-        typeof(GenerateChatCompletionResponseDoneReasonNullableJsonConverter),
     ])]
 [JsonSerializable(typeof(JsonSerializerContextTypes))]
 internal sealed partial class SourceGenerationContext : JsonSerializerContext;
