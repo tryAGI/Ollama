e395d169895b3a4161eb7765fe1a63332c7c9ec8
HavenDV <havendv@gmail.com>
2024-08-13 10:41:45 +0400

    test: Removed Mocked tests.

--- src/tests/Ollama.IntegrationTests/Tests.Helpers.cs
+++ src/tests/Ollama.IntegrationTests/Tests.Helpers.cs
@@ -1,46 +1,4 @@
-using System.Net;
-using System.Text.Json;
-using Moq;
-using Moq.Protected;
-
 namespace Ollama.IntegrationTests;
 
 [TestClass]
-public partial class Tests
-{
-    public static OllamaApiClient MockApiClient(MessageRole role, string answer)
-    {
-        return MockApiClient(new HttpResponseMessage
-        {
-            StatusCode = HttpStatusCode.OK,
-            Content = new StringContent(JsonSerializer.Serialize(new GenerateChatCompletionResponse
-            {
-                Done = true,
-                DoneReason = DoneReasonEnum.Stop,
-                Message = new Message
-                {
-                    Role = role,
-                    Content = answer,
-                },
-                CreatedAt = DateTime.UtcNow,
-                Model = "model",
-            }))
-        });
-    }
-    
-    public static OllamaApiClient MockApiClient(HttpResponseMessage response)
-    {
-        var mockHandler = new Mock<HttpMessageHandler>(MockBehavior.Strict);
-
-        mockHandler
-            .Protected()
-            .Setup<Task<HttpResponseMessage>>(
-                "SendAsync",
-                ItExpr.Is<HttpRequestMessage>(_ => true),
-                ItExpr.IsAny<CancellationToken>())
-            .ReturnsAsync(() => response);
-
-        var httpClient = new HttpClient(mockHandler.Object) { BaseAddress = new Uri("http://127.0.0.1/") };
-        return new OllamaApiClient(httpClient);
-    }
-}
+public partial class Tests;
