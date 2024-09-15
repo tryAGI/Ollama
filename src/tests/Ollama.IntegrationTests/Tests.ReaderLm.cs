0c9fae6ceeceaa07dd57f345f6fa492bb1ddbff4
HavenDV <havendv@gmail.com>
2024-09-15 07:17:54 +0400

    docs: Added ReaderLm case.

---
+++ src/tests/Ollama.IntegrationTests/Tests.ReaderLm.cs
@@ -0,0 +1,34 @@
+namespace Ollama.IntegrationTests;
+
+public partial class Tests
+{
+    [TestMethod]
+    public async Task ReaderLm()
+    {
+#if DEBUG
+        await using var container = await PrepareEnvironmentAsync(EnvironmentType.Local, "reader-lm:latest");
+#else
+        await using var container = await PrepareEnvironmentAsync(EnvironmentType.Container, "reader-lm:latest");
+#endif
+
+        var enumerable = container.ApiClient.Completions.GenerateCompletionAsync(
+            "reader-lm:latest", 
+            """
+            <html>
+              <body>
+                <h3>Why is the sky blue?</h3>
+                <p>The sky appears blue because of the way light from the sun is reflected by the atmosphere. The atmosphere is made up of gases, including nitrogen and oxygen, which scatter light in all directions. This scattering causes the sunlight to appear as a rainbow of colors, with red light scattered more than other colors.
+                </p>
+              </body>
+            </html>
+            """);
+        await foreach (var response in enumerable)
+        {
+            Console.Write(response.Response);
+        }
+        
+        // ### Why is the sky blue?
+        // 
+        // The sky appears blue because of the way light from the sun is reflected by the atmosphere. The atmosphere is made up of gases, including nitrogen and oxygen, which scatter light in all directions. This scattering causes the sunlight to appear as a rainbow of colors, with red light scattered more than other colors.
+    }
+}
