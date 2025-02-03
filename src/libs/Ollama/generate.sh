a47c83ae3e87fb48a8c5a297e1c69c2f035fead8
Dustin Reagan <drreagan@gmail.com>
2025-02-02 18:55:32 -0600

    oops, uncomment out lines in generate.sh and add test

--- src/libs/Ollama/generate.sh
+++ src/libs/Ollama/generate.sh
@@ -1,7 +1,7 @@
 dotnet tool install --global autosdk.cli --prerelease
 rm -rf Generated
-#curl -o openapi.yaml https://raw.githubusercontent.com/davidmigloz/langchain_dart/main/packages/ollama_dart/oas/ollama-curated.yaml
-#dotnet run --project ../../helpers/FixOpenApiSpec openapi.yaml
+curl -o openapi.yaml https://raw.githubusercontent.com/davidmigloz/langchain_dart/main/packages/ollama_dart/oas/ollama-curated.yaml
+dotnet run --project ../../helpers/FixOpenApiSpec openapi.yaml
 if [ $? -ne 0 ]; then
  echo "Failed, exiting..."
  exit 1
