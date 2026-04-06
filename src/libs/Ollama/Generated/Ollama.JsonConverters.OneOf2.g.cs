5728b4378e6ba8bb534d5455b7f939fd49e1a2f9
Konstantin S. <havendv@gmail.com>
2026-04-06 20:03:36 +0400

    feat: Updated OpenAPI spec (#194)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Ollama/Generated/Ollama.JsonConverters.OneOf2.g.cs
+++ src/libs/Ollama/Generated/Ollama.JsonConverters.OneOf2.g.cs
@@ -3,7 +3,7 @@
 namespace Ollama.JsonConverters
 {
     /// <inheritdoc />
-    public class OneOfJsonConverter<T1, T2> : global::System.Text.Json.Serialization.JsonConverter<global::Ollama.OneOf<T1, T2>>
+    public class OneOfJsonConverter<[global::System.Diagnostics.CodeAnalysis.DynamicallyAccessedMembers(global::System.Diagnostics.CodeAnalysis.DynamicallyAccessedMemberTypes.PublicProperties)] T1, [global::System.Diagnostics.CodeAnalysis.DynamicallyAccessedMembers(global::System.Diagnostics.CodeAnalysis.DynamicallyAccessedMemberTypes.PublicProperties)] T2> : global::System.Text.Json.Serialization.JsonConverter<global::Ollama.OneOf<T1, T2>>
     {
         /// <inheritdoc />
         public override global::Ollama.OneOf<T1, T2> Read(
